3fc9cc76eed0896b939f83751a911b152a978dc1
santa <santa@Eli>
2026-05-12 14:59:46 -0700

    aqui ponemos comentarios como lo que se hizo o lo que falta para indicar que aun no esta terminado o asi

--- src/tienda/regisservicio.java
+++ src/tienda/regisservicio.java
@@ -6,3 +6,6 @@ public class regisservicio {
     }
  
 }
+//*cada que se haga un cambio y lo guardemos con ctrl + s
+//* se va a subrayar de color la clase 
+//*para guardar primero arriba a team luego commit
