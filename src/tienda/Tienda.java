15dd450a9c9da192dc3bd78754142241e8b1d882
santa <santa@Eli>
2026-05-13 18:16:29 -0700

    LISTO :>

--- src/tienda/Tienda.java
+++ src/tienda/Tienda.java
@@ -4,8 +4,11 @@ public class Tienda {
     public static void main(String[] args) {
        
         Scanner num = new Scanner(System.in);
-        int opcion;
+        int opcion = 0;
+        boolean texto = false;
+       do{
         do {
+            try{
         System.out.println("\nINVENTARIO TIENDA");
         System.out.println("1.Agregar un nuevo producto");
         System.out.println("2.Eliminar un producto no deseado");
@@ -18,8 +21,24 @@ public class Tienda {
         System.out.println("0.Salir");
         
          System.out.println("Elija una opcion: ");
-         opcion = num.nextInt();
-         
+         String opcionn = num.nextLine();
+           if(opcionn.matches("[0-9]+")){
+                texto = false; 
+                 opcion = Integer.parseInt(opcionn);
+                 
+                 if(opcion >8 || opcion <0){
+                      System.out.println("No existe esa opcion");
+                       System.out.println("intente nuevamente");
+                 }
+                    }else{
+           throw new EntradaInvalidaExcepcion("Solo numeros");
+                 }
+            }catch(EntradaInvalidaExcepcion e){
+                texto = true;
+                System.out.println(e.getMessage());
+            }
+        }while(texto);
+        
          switch(opcion){
              case 1:
                  catalogotienda.main(null);
@@ -34,16 +53,20 @@ public class Tienda {
                  listadoprod.main(null);
                  break;
              case 5:
+                 regisservicio.main(null);
                  break;
              case 6:
+                 eliminarservicio.main(null);
                  break;
              case 7:
+                 mostrarlistadeservicios.main(null);
                  break;
              case 8:
+                 Materialesdisponibles.main(null);
                  break;
          }
         
     }while(opcion != 0);
-    
 }
+
 }
